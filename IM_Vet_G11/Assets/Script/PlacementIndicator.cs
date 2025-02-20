09f251d9328113804bfae361e5a157dd5b726749
mikaelalevin <levinmikaela@gmail.com>
2025-02-20 14:45:57 +0100

    Workshop_5

--- IM_Vet_G11/Assets/Script/PlacementIndicator.cs
+++ IM_Vet_G11/Assets/Script/PlacementIndicator.cs
@@ -2,12 +2,14 @@ using UnityEngine;
 using UnityEngine.XR.ARFoundation;
 using UnityEngine.XR.ARSubsystems;
 using System.Collections.Generic;
+using TMPro;
 
 public class PlacementIndicator : MonoBehaviour
 {
     public ARRaycastManager rayManager;
-    public GameObject horse;  // Reference to the horse object that will move
 
+    public GameObject horseScene;
+    public GameObject text;
     private GameObject visual;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
@@ -18,6 +20,9 @@ public class PlacementIndicator : MonoBehaviour
 
         // Hide the placement indicator visual initially
         visual.SetActive(false);
+        text.SetActive(false);
+        
+        horseScene.SetActive(false);
     }
 
     // Update is called once per frame
@@ -43,20 +48,26 @@ public class PlacementIndicator : MonoBehaviour
         }
 
         // Detect screen tap or click
-        if (Input.touchCount > 0 || Input.GetMouseButtonDown(0))
+        if (Input.GetMouseButtonDown(0))
         {
+            //text.SetActive(true); (debugg för att testa input)
+            
+            horseScene.SetActive(true);
+
             // Only process the first touch or mouse click
             Vector2 touchPosition = (Input.touchCount > 0) ? Input.GetTouch(0).position : (Vector2)Input.mousePosition;
 
             // Check if the PlacementIndicator has a valid position
-            if (horse != null && hits.Count > 0)
+            if (horseScene != null && hits.Count > 0)
             {
                 // Move the horse to the PlacementIndicator's position
-                horse.transform.position = hits[0].pose.position;
-                horse.transform.rotation = hits[0].pose.rotation; // Optionally match the rotation
+                
+
+                horseScene.transform.position = hits[0].pose.position;
+                horseScene.transform.rotation = hits[0].pose.rotation; // Optionally match the rotation
 
                 // Log to verify the movement
-                Debug.Log("Horse moved to: " + horse.transform.position);
+                Debug.Log("Horse moved to: " + horseScene.transform.position);
             }
         }
     }
